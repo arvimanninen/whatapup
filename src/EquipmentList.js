a3dada007909be28c00187f8d961b6f8ef306b8c
arvimanninen <arvi.manninen@myy.haaga-helia.fi>
2018-06-06 19:16:04 +0300

    key attribute added to EquipmentList items

--- src/EquipmentList.js
+++ src/EquipmentList.js
@@ -4,13 +4,11 @@ import ReactDOM from 'react-dom';
 class EquipmentList extends React.Component {
     render(){
         const category = this.props.category;
-        const items = category.items.map((item) => <li>{item.name}</li>);
+        const items = category.items.map((item) => <li key={item.name}>{item.name}</li>);
         return(
             <div>
                 <h3>{category.name}</h3>
-                <ul>
-                    {items}
-                </ul>
+                <ul>{items}</ul>
                 <button onClick={() => this.props.handleViewChange("MainMenu")}>Back</button>
             </div>
         );
