8181af5b999d07cf82c5d35483ef764dc10c4f72
arvimanninen <arvi.manninen@myy.haaga-helia.fi>
2018-07-05 17:57:16 +0300

    EquipmentItemButton created, not integrated to the EquipmentList yet

--- src/EquipmentList.js
+++ src/EquipmentList.js
@@ -1,21 +1,21 @@
 import React from 'react';
 
-class EquipmentList extends React.Component {
-    // TODO: CHANGE FUNCTION NAME!
-    
-
+class EquipmentList extends React.Component {    
     render(){
         const category = this.props.category;
         let itemButtons = [];
         for(let i = 0; i < category.items.length; i++) {
             let item = category.items[i];
+            //  item.completed ? 
+            //      itemButtons.unshift(<EquipmentItemButton completed=true categoryName={category.name} itemName={item.name} handleClick={this.props.handleItemClick}>)
+            //      : itemButtons.push(<EquipmentItemButton completed=false categoryName={category.name} itemName={item.name}>)
             if(item.completed === false) {
                 itemButtons.unshift(
                     <button className="btn btn-primary btn-lg btn-block" key={item.name} onClick={() => this.props.handleItemClick(category.name, item.name)}>
                         {item.name}
                     </button>
                 );
-            } else if (item.completed === true){
+            } else if(item.completed === true) {
                 itemButtons.push(
                     <button className="btn btn-light btn-lg btn-block" key={item.name} onClick={() => this.props.handleItemClick(category.name, item.name)}>
                         <s>{item.name}</s>
@@ -23,19 +23,15 @@ class EquipmentList extends React.Component {
                 );
             }
         }
-        /*
-        const itemButtons = category.items.map((item) =>
-                <button className="btn btn-primary btn-lg" key={item.key} onClick={() => 
-                this.props.handleItemClick(item.key)}>{item.name}</button>
-        );
-        */
         return(
             <div className="container-fluid">
                 <div className="row">
                     <div className="col-sm-1 col-md-2 col-lg-3"></div>
                     <div className="col">
-                        <h3>{category.name}</h3><br/>
-                        <div>{itemButtons}</div><br/>
+                        <h3>{category.name}</h3>
+                        <br/>
+                        <div>{itemButtons}</div>
+                        <br/>
                         <button className="btn btn-secondary" onClick={() => this.props.handleViewChange("MainMenu")}>Back</button>
                     </div>
                     <div className="col-sm-1 col-md-2 col-lg-3"></div>
@@ -43,6 +39,30 @@ class EquipmentList extends React.Component {
             </div>
         );
     }
+}
+
+class EquipmentItemButton extends React.Component {
+    render() {
+        const completed = this.props.completed;
+        const categoryName = this.props.categoryName;
+        const itemName = this.props.itemName;
+        // TODO: ADD handleClick
+        return(
+            <React.Fragment>
+                {completed === true ?
+                    <button className="btn btn-light btn-lg btn-block" key={itemName} onClick={() => this.props.handleClick(categoryName, itemName)}>
+                            <s>{itemName}</s>
+                    </button> 
+                    : 
+                    <button className="btn btn-primary btn-lg btn-block" key={itemName} onClick={() => this.props.handleClick(categoryName, itemName)}>
+                            {itemName}
+                    </button>
+                }
+            </React.Fragment>
+        );
+    }
+    
+
 }
 
 export default EquipmentList;
